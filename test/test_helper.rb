cf0225c3845e465c9bed047ec4918aec6cf0ea04
Flavien Raynaud <flavien.raynaud@gmail.com>
2015-05-05 00:04:56 +0200

    install FactoryGirl and SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,13 @@
+require 'simplecov'
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # Check that there is no pending migration, if so alert and halt
+  ActiveRecord::Migration.check_pending!
 
-  # Add more helper methods to be used by all tests here...
+  include FactoryGirl::Syntax::Methods
 end
