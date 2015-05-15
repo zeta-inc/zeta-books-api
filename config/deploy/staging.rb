c6a9212afa031ace38315a4a860e559143f9845f
Thomas Papillon <tp@irrealis.me>
2015-05-15 21:02:12 +0200

    update staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -68,6 +68,6 @@ server 'ec2-52-25-29-38.us-west-2.compute.amazonaws.com',
        user: 'zeta-books-api',
        roles: %w{web app db},
        ssh_options: {
-         keys: %w(private/zeta.pem),
+         keys: %w(../zeta.pem),
          forward_agent: true,
        }
